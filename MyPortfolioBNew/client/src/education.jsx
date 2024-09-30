392acc825a9ba83e7c347cda3e2099a2d40651fb
FatimaMawaj <Fmawaj@my.centennialcollege.ca>
2024-09-29 22:27:00 -0400

    Added Education component with MyButton functionality

--- MyPortfolioBNew/client/src/education.jsx
+++ MyPortfolioBNew/client/src/education.jsx
@@ -1,7 +1,20 @@
+function MyButton() {
+     function handleClick() {
+       alert('You clicked me!');
+     }
+   
+     return (
+       <button onClick={handleClick}>
+         Click me
+       </button>
+     );
+   }
+
 export default function Education() {
      return (
      <>
     <p>Education.Qualification</p>
+    <MyButton />
      </>
      );
      }
